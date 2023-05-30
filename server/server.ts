7764dafcf7edf99a326efe98fb50dc201a52a49e
Elliott Coleman <ejaycoleman@googlemail.com>
2023-05-30 21:21:46 +0100

    cleanup

--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,4 @@
-import express from "express";
-
-const router = <T>(routes: { [K in keyof T]: T[K] }) => {
-  return routes;
-};
-
-export type Get = { callback: () => string | string[]; type: "get" }; // any valid route response
-export type Post = { callback: () => string | string[]; type: "post" }; // any valid route response
-
-const get = (getCallback: () => string | string[]): Get => {
-  return { callback: getCallback, type: "get" };
-};
-
-const post = (getCallback: () => string | string[]): Post => {
-  return { callback: getCallback, type: "post" };
-};
+import { createHTTPServer, get, post, router } from "./library";
 
 const appRouter = router({
   getValues: get(() => {
@@ -27,18 +12,6 @@ const appRouter = router({
 });
 
 export type AppRouter = typeof appRouter; // for client
-
-const createHTTPServer = ({ router }: { router: { [key: string]: any } }) => {
-  const app = express();
-
-  Object.entries(router).map(([routeName, routeFunction]) => {
-    app.get("/" + routeName, (req, res) => {
-      res.send(routeFunction());
-    });
-  });
-
-  return app;
-};
 
 const server = createHTTPServer({
   router: appRouter,
